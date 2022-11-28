f4949df6c5b82c923ffdd94ecbfe57e2c115eabf
Engin Diri <engin.diri@ediri.de>
2022-11-28 16:03:41 +0100

    feat: initial version

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"github.com/pulumi/pulumi-civo/sdk/v2/go/civo"
+	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
+)
+
+func main() {
+	pulumi.Run(func(ctx *pulumi.Context) error {
+
+		instance, err := civo.NewInstance(ctx, "test", &civo.InstanceArgs{
+			Hostname:         pulumi.String("test"),
+			Region:           pulumi.String("FRA1"),
+			Size:             pulumi.String("g3.large"),
+			PublicIpRequired: pulumi.String("create"),
+			InitialUser:      pulumi.String("root"),
+			DiskImage:        pulumi.String("19c4c893-b452-4bcc-a3f2-42f8204a36ac"),
+		})
+		if err != nil {
+			return err
+		}
+
+		ctx.Export("ip", instance.PublicIp)
+
+		return nil
+	})
+}
